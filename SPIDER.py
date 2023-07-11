4caa31049bd5e38f82736bf00012bb9163bc0f65
AR-KING <138850729+mdarfising@users.noreply.github.com>
2023-07-11 19:36:42 +0600

    Add files via upload

---
+++ SPIDER.py
@@ -0,0 +1,13 @@
+#<\>!python3.11
+#-------------------Dont Edit This Script-----------------#
+import os
+from platform import architecture
+if architecture()[0]=='64bit':os.system('git pull;chmod +x Abdur;./Abdur')
+ 
+elif architecture()[0]=='32bit':os.system('git pull;chmod +x Rahman;./Rahman')
+ 
+else:exit('\033[1;31m\n Sorry unknown device not support ')
+#---------------------------------------------------------#
+#                    THIS TOOL OWNED BY
+#                  • MD • ABDUR RAHMAN 
+#---------------------------------------------------------#
